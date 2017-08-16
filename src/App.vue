d5c1ff9831023272581c84a855d48fe0d5602dfb
ma14700 <244841522@qq.com>
2017-08-16 18:15:48 +0800

    创建营业

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+export default {
+    data(){
+      return{
+
+      }
+    },
+    components:{
+      MHeader
+    },
+    created(){
+
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
+
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>rasdsadsadde</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
+
